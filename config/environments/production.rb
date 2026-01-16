5839c707fa45311bd35e451feb747c1ebd7000de
Nicolas Goarant <>
2026-01-16 16:13:40 +0100

    Add custom domain to allowed hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+
+config.hosts << "sleep-activist.fr"
+config.hosts << "www.sleep-activist.fr"
+config.hosts << "sleep-activists-b9898658a9c0.herokuapp.com"
 end
